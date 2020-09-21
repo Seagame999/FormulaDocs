e0123428d60fe6319e4b9605b9085fcdba1c0a06
Sittinon SUPAPBURUS <sittinon.s@live.ku.th>
2020-09-21 16:16:15 +0700

    Commit 21/9/63.

--- FormulaDocs/Controllers/HomeController.cs
+++ FormulaDocs/Controllers/HomeController.cs
@@ -1,30 +1,24 @@
-﻿using System;
+﻿using FormulaDocs.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.IO;
+using System.Data.Entity.Validation;
 
 namespace FormulaDocs.Controllers
 {
     public class HomeController : Controller
     {
+        FormulaDocsDBEntities db = new FormulaDocsDBEntities();
+
         public ActionResult Index()
         {
-            return View();
-        }
+            var publicRelationResult = db.PublicRelation.ToList().LastOrDefault();
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(publicRelationResult);
         }
     }
 }
---
+++ FormulaDocs/Controllers/PublicRelationController.cs
@@ -0,0 +1,54 @@
+﻿using FormulaDocs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.IO;
+using System.Data.Entity.Validation;
+
+namespace FormulaDocs.Controllers
+{
+    public class PublicRelationController : Controller
+    {
+        FormulaDocsDBEntities db = new FormulaDocsDBEntities();
+
+        // GET: PublicRelation
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult CreateNews()
+        {
+            if (Session["Role"] != null && Session["Role"].Equals("Admin"))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CreateNews(PublicRelation publicRelation)
+        {
+            if (ModelState.IsValid)
+            {
+                publicRelation.Creator = Session["Name"].ToString();
+                publicRelation.Date = DateTime.Today;
+                publicRelation.DataIsActive = true;
+
+                db.PublicRelation.Add(publicRelation);
+                db.SaveChanges();
+                ModelState.Clear();
+
+                return RedirectToAction("Index","Home");
+            }
+
+            return View(publicRelation);
+        }
+    }
+}
